0c6c024030098e45d476875afd465cd4559954ef
61090049 <61090049@kmitl.ac.th>
2022-05-24 02:02:41 +0700

    Fixed Webview Redirection

--- app/src/main/java/com/example/hotelautomtionproject/presentation/presentation/WebViewActivity.kt
+++ app/src/main/java/com/example/hotelautomtionproject/presentation/presentation/WebViewActivity.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -20,8 +22,17 @@ class WebViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
          binding = ActivityWebViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.wvInfo.apply {
-            webViewClient = WebViewClient()
+            webViewClient = object: WebViewClient(){
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    url: String
+                ): Boolean {
+                    view?.loadUrl(url)
+                    return false
+                }
+            }
             if(getUrlTest().isNotEmpty()) {
                 loadUrl(getUrlTest())
             }
